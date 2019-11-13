cffa67e8b5bd731a4aa3fd500a02318b76bf6693
rneelams <ramesh.neelamsetti@capgemini.com>
2019-11-13 15:26:40 +0530

    updated login page and signup page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { SignupPageComponent } from "./signup-page/signup-page.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomePageComponent },
+  { path: "login", component: LoginPageComponent },
+  { path: "register", component: SignupPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-signup-page',
-  templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  selector: "app-signup-page",
+  templateUrl: "./signup-page.component.html",
+  styleUrls: ["./signup-page.component.css"]
 })
 export class SignupPageComponent implements OnInit {
+  constructor(private route: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSignupButtonclicked() {
+    this.route.navigate(["/home"]);
   }
-
 }
